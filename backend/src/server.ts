c20625529813e9104cb3c8ec6d862744ba680ca1
fnd.davi <fernando.ferreira32@fatec.sp.gov.br>
2025-03-15 17:20:22 -0300

    init server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import { connectDB } from './database';
+
+connectDB();
 
 const app = express();
 const PORT = 3000;
 
+//inserir uma requisicao
 app.get("/", (req, res) => {
   res.send("Hello, Backend com TypeScript!");
 });
