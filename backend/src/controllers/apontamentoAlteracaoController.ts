babbded2e30ab4a420a2f228bbaedcc449003e0b
jummanso <julianamaciel97@gmail.com>
2025-04-23 19:39:07 -0300

    backend schedule

--- backend/src/controllers/apontamentoAlteracaoController.ts
+++ backend/src/controllers/apontamentoAlteracaoController.ts
@@ -1,16 +1,27 @@
 import { Request, Response } from 'express';
-import ApontamentoAlteracaoModel from '../models/apontamentoAlteracaoModel';
 
 class ApontamentoAlteracao {
-    public async getAll(req: Request, res: Response): Promise<Response> {
-        try {
-            const alteracoes = await ApontamentoAlteracaoModel.find({});
-            return res.status(201).json(alteracoes)
-
-        } catch (error: any) {
-            return res.status(500).json({ err: error.message });
-        }
+  public async getAll(req: Request, res: Response): Promise<Response> {
+    try {
+      // Lógica para buscar dados do Mongo
+      return res.status(200).json({ message: 'GET route funcionando' });
+    } catch (error: any) {
+      return res.status(500).json({ err: error.message });
     }
+  }
+
+  public async receiveFilter(req: Request, res: Response): Promise<Response> {
+    try {
+      const { startDate, endDate, latitude, longitude } = req.body;
+
+      console.log('Dados recebidos do front-end:');
+      console.log({ startDate, endDate, latitude, longitude });
+
+      return res.status(200).json({ message: 'Dados recebidos com sucesso!' });
+    } catch (error: any) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
 }
 
 export default new ApontamentoAlteracao();
--- backend/src/routes/apontamento.routes.ts
+++ backend/src/routes/apontamento.routes.ts
@@ -7,5 +7,6 @@ const apontamentoRoute = Router();
 
 // Rota para obter todas as alterações
 apontamentoRoute.get('/', ApontamentoAlteracao.getAll);
+apontamentoRoute.post('/filtro', ApontamentoAlteracao.receiveFilter); // Nova rota POST
 
 export default apontamentoRoute;
